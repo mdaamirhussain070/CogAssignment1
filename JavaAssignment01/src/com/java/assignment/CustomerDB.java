3ebe5259055a00b8328020558937fa7f259e3ee5
mdaamirhussain070 <56185311+mdaamirhussain070@users.noreply.github.com>
2022-11-20 22:08:21 +0530

    finally completed

---
+++ JavaAssignment01/src/com/java/assignment/CustomerDB.java
@@ -0,0 +1,19 @@
+package com.java.assignment;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CustomerDB {
+	public List<Customer> getCustomerList(){
+		Customer c1=new Customer(1,"Tom","male","Bengalore");
+		Customer c2=new Customer(2,"Mike","male","Delhi");
+		Customer c3=new Customer(3,"Miley","femal","Pune");
+		Customer c4=new Customer(4,"Kunal","male","Delhi");
+		Customer c5=new Customer(5,"Sakshi","male","Delhi");
+		
+		List<Customer> customerList=Arrays.asList(c1,c2,c3,c4,c5);
+		return customerList;
+	}
+
+
+}
